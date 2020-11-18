19571d59922ecc69988f40b2427a091b589a0381
Krishna Narayan <>
2020-11-17 18:28:19 -0800

    fixed script issues

--- username.sh
+++ username.sh
@@ -3,9 +3,10 @@
 # Krishna Narayan
 echo "Enter a username: "
 read -r NAME
-while echo "$NAME" | grep -E -v "^[0-9a-z_]{2,11}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z]{1}[0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
 	echo "Please enter only lower case letters, digits, or _"
+	echo "The first character MUST be a lower case letter!"
 	echo "Your username must contain 3-12 characters!"
 	read -r NAME
 done
